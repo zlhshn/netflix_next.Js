f56c5dc66e90d098d4bc214cf37f6955b0a42c41
zlhshn <zsahin4401@gmail.com>
2024-02-02 14:59:42 +0300

    feat : project structure created

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,12 +7,14 @@ module.exports = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      colors: {
+        "gray-dark-main": "#23242a",
+        "gray-dark-second": "#28292d",
+        "gray-light": "#d3dce6",
+        "red-main": "#ff4b45",
       },
     },
   },
+  darkMode: "class",
   plugins: [],
 };
